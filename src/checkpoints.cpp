f39f66e1f3c454e571b8e8581fac1402fa7d3158
cinnamoncoin <cinnamon.carter@aol.com>
2014-09-06 17:33:49 +0000

    fix

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -38,7 +38,8 @@ namespace Checkpoints
         (   27010, uint256("0x0236a9861ab090d2207531313abaac09637cfc0f45493d635928a542100f9037"))
         (   27850, uint256("0x5a16e9fc2e680d509258b611bb030fce83e1f55b95012b27ebc401ed0405dfda"))
 		(   58005, uint256("0x0a6ba594978482d0ba890ce53a83cf20b8aa1cbff7a0971931f875c9fa78c56f"))
-		;
+
+            ;
 
 
     static MapCheckpoints mapCheckpointsTestnet =
